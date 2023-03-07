292459dfc854ec9dab4d4d8c1f02b87e6ea5d1c8
Maijs Garais <maireadsbooks@gmail.com>
2023-03-07 12:08:48 +0000

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Palette from "./Palette";
 import seedColours from "./seedColours";
 import generatePalette from "./colourHelpers";
+import { Route, Routes } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function App() {
+  function GetPalette() {
+    const { id } = useParams();
+    const found = seedColours.find(p => {
+      return p.id === id;
+    });
+    const palette = generatePalette(found);
+    return <Palette palette={palette} />;
+  }
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColours[1])} />
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Mini palettes</h1>} />
+      <Route path="/palette/:id" element={<GetPalette />} />
+    </Routes>
   );
 }
 
