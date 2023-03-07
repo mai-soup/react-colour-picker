7da6ce9d3ac1afabeba1864bbafe954733120b62
Maijs Garais <maireadsbooks@gmail.com>
2023-03-07 05:34:01 +0000

    add colourbox styles

--- src/ColourBox.jsx
+++ src/ColourBox.jsx
@@ -2,9 +2,14 @@ import "./ColourBox.css";
 
 const ColourBox = ({ background, name }) => {
   return (
-    <div style={{ background: background }} className="ColourBox">
-      <span>{name} </span>
-      <span>MORE</span>
+    <div style={{ background }} className="ColourBox">
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">MORE</span>
     </div>
   );
 };
