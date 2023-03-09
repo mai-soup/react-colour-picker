62494af9ad412ae2e8cadf90228d99766f0bf107
Maijs Garais <maireadsbooks@gmail.com>
2023-03-09 18:08:01 +0000

    add transitions when switching routes, fix dependency issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Palette from "./Palette";
 import seedColours from "./seedColours";
 import generatePalette from "./colourHelpers";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes, useParams, useLocation } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColourPalette from "./SingleColourPalette";
 import NewPaletteForm from "./NewPaletteForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
@@ -55,26 +55,43 @@ function App() {
     window.localStorage.setItem("palettes", JSON.stringify(updatedPalettes));
   };
 
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage("fadeOut");
+  }, [location, displayLocation]);
+
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <PaletteList palettes={palettes} handleDelete={handleDelete} />
+    <div
+      className={`${transitionStage}`}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setTransistionStage("fadeIn");
+          setDisplayLocation(location);
         }
-      />
-      <Route
-        path="/palette/new"
-        element={
-          <NewPaletteForm savePalette={savePalette} palettes={palettes} />
-        }
-      />
-      <Route path="/palette/:id" element={<GetPalette />} />
-      <Route
-        path="/palette/:paletteId/:colourId"
-        element={<GetSingleColourPalette />}
-      />
-    </Routes>
+      }}
+    >
+      <Routes location={displayLocation}>
+        <Route
+          path="/"
+          element={
+            <PaletteList palettes={palettes} handleDelete={handleDelete} />
+          }
+        />
+        <Route
+          path="/palette/new"
+          element={
+            <NewPaletteForm savePalette={savePalette} palettes={palettes} />
+          }
+        />
+        <Route path="/palette/:id" element={<GetPalette />} />
+        <Route
+          path="/palette/:paletteId/:colourId"
+          element={<GetSingleColourPalette />}
+        />
+      </Routes>
+    </div>
   );
 }
 
