7b1ddb21076b102bd7620d7fa2f23c7abc720bfa
Maijs Garais <maireadsbooks@gmail.com>
2023-03-09 13:08:31 +0000

    fix chevron displaying in middle not side

--- src/styles/NewPaletteForm.styles.jsx
+++ src/styles/NewPaletteForm.styles.jsx
@@ -45,6 +45,7 @@ export const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   padding: theme.spacing(0, 1),
+  width: "100%",
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
   justifyContent: "flex-end",
